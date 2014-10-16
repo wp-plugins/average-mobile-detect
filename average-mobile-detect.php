0cff1fbd7e4b199b38cae81e22108f97e442c362
average.technology <average.technology@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-10-16 01:29:15 +0000

    Added tag of version 1.1
    git-svn-id: https://plugins.svn.wordpress.org/average-mobile-detect/tags/1.1@1008310 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ average-mobile-detect.php
@@ -0,0 +1,404 @@
+<?php
+/*
+    Plugin Name: Average Mobile Detect
+    Plugin URI:
+    Description: Redirects mobile traffic to your mobile website on a page-by-page basis (posts and custom post types included). This can be overridden on any page individually with a convenient meta box adjacent to the WYSIWYG. Sets a cookie to remember which version of your website (desktop or mobile, usually) your visitors opted for. Includes a widget for inserting a link back to your mobile site, which is only generated for mobile devices. Includes two shortcodes for generating links to your mobile site--one is generated only for mobile devices and the other is generated regardless. No CSS rules are used. CSS classes are provided, yielding coders full reign to style the generated links to fit the website theme. Adds a class to the body ("mobile-detected") to help coders write styles specifically for mobile devices. Leaves 404 errors untouched, allowing you to maintain 404 statuses. Basically, it gives you loads of control of your mobile redirects.
+    Version: 1.1
+    Author: Average
+    Author URI: http://profiles.wordpress.org/averagetechnology
+    License: Public Domain
+        ___
+       /   |_   _____  _________ _____ ____
+      / /| | | / / _ \/ ___/ __ `/ __ `/ _ \
+     / ___ | |/ /  __/ /  / /_/ / /_/ /  __/
+    /_/  |_|___/\___/_/   \__,_/\__, /\___/
+                               /____/     ™
+                               by Joe Rhoney
+*/
+
+
+
+/*
+    R E D I R E C T
+    ===============
+*/
+
+add_filter('template_redirect','avrgmobdtctRedirect');
+function avrgmobdtctRedirect() {
+  if((get_option('avrgmobdtct_redirect')==1)&&(!is_404())){
+    if(!function_exists("remove_http")){
+      function remove_http($url){
+        $disallowed = array('http://','https://');
+        foreach($disallowed as $d){
+          if(strpos($url,$d)===0){
+            return str_replace($d,'',$url);
+          }
+        }
+        return $url;
+      }
+    }
+
+    $avrgmobdtct_equiv = get_post_meta(get_the_ID(),'avrgmobdtct_equiv',true);
+    if(!empty($avrgmobdtct_equiv)){
+      $pageURN = $avrgmobdtct_equiv;
+    }
+    else
+    {
+      $pageURN = $_SERVER['REQUEST_URI'];
+    }
+
+    if(get_option('the_mobile_site_uri')){
+      $the_mobile_site_uri=get_option('the_mobile_site_uri');}
+    else{
+      $the_mobile_site_uri=0;}
+
+    $xhttp_the_mobile_site_uri = remove_http($the_mobile_site_uri);
+
+    if(get_option('non_mobile_site_uri')){
+      $non_mobile_site_uri=remove_http(get_option('non_mobile_site_uri'));}
+    else{
+      $non_mobile_site_uri=0;}
+
+    if(!class_exists('Mobile_Detect')){
+      include plugin_basename('/Mobile_Detect.php');}
+
+    $detect = new Mobile_Detect();
+    if ($detect->isMobile() && !$detect->isTablet())
+    {
+      $want_mobile=1;
+      if(isset($_COOKIE['mobile']))
+      {
+        if($_COOKIE['mobile']=="true")
+        {
+          if(isset($_SERVER['HTTP_REFERER']))
+          {
+            $referer = $_SERVER['HTTP_REFERER'];
+            if(strpos($referer,$xhttp_the_mobile_site_uri))
+            {
+              $want_mobile=0;
+              setcookie("mobile","false",0,"/",$non_mobile_site_uri);
+            }
+          }
+        }
+        else
+        {
+          $want_mobile=0;
+        }
+      }
+      else
+      {
+        $want_mobile=1;
+        setcookie("mobile","true",0,"/",$non_mobile_site_uri);
+      }
+      if ($want_mobile==1)
+      {
+        header('Location: '.$the_mobile_site_uri.$pageURN,true,302);
+      }
+      if ($want_mobile==0)
+      {
+        function add_class_mobile($class) {
+          $class[] = 'mobile-detected';
+          return $class;
+        }
+        add_filter('body_class','add_class_mobile');
+      }
+    }
+  }
+}
+
+
+
+/*
+    S E T T I N G S   P A G E
+    =========================
+*/
+
+# Remove default value before saving to the database
+function xdefaultvalue($input)
+{
+  if(isset($input))
+  {
+    if ($input=='http://')
+    {
+      $input = NULL;
+      return $input;
+    }
+    else
+    {
+      return $input;
+    }
+  }
+}
+if(!class_exists('avrgmobdtct_class')) :
+define('avrgmobdtct_ID', 'avrgmobdtct');
+define('avrgmobdtct_NICK', 'Mobile Detect');
+  class avrgmobdtct_class
+  {
+    public static function file_path($file)
+    {
+      return ABSPATH.'wp-content/plugins/'.str_replace(basename( __FILE__),"",plugin_basename(__FILE__)).$file;
+    }
+    public static function register()
+    {
+      register_setting(avrgmobdtct_ID.'_options', 'avrgmobdtct_redirect');
+      register_setting(avrgmobdtct_ID.'_options', 'the_mobile_site_uri','xdefaultvalue');
+      register_setting(avrgmobdtct_ID.'_options', 'non_mobile_site_uri');
+    }
+    public static function menu()
+    {
+      add_options_page(avrgmobdtct_NICK.' Plugin Options', avrgmobdtct_NICK, 'manage_options', avrgmobdtct_ID.'_options', array('avrgmobdtct_class', 'options_page'));
+    }
+    public static function options_page()
+    {
+      if (!current_user_can('manage_options'))
+      {
+        wp_die(__('You do not have sufficient permissions to access this page.'));
+      }
+      $avrgmobdtctID = avrgmobdtct_ID;
+      include(self::file_path('options.php'));
+    }
+  }
+  if (is_admin())
+  {
+    add_action('admin_init', array('avrgmobdtct_class','register'));
+    add_action('admin_menu', array('avrgmobdtct_class','menu'));
+  }
+endif;
+
+
+
+/*
+    W I D G E T
+    ===========
+*/
+
+class avrg_mobsitelink_widget extends WP_Widget {
+  function __construct()
+  {
+    parent::__construct(
+      'mobsitelink', // Base ID
+      'Mobile Site Link', // Name
+      array('description'=> __('A link that goes to the mobile website as set in the Mobile Detect settings and displays itself only on mobile phones.', 'text_domain' ),) // Args
+    );
+  }
+#   Front-end display of widget
+  public function widget($args,$instance)
+  {
+    if(!class_exists('Mobile_Detect'))
+    {
+      include plugin_basename('/Mobile_Detect.php');
+    }
+    $detect = new Mobile_Detect();
+    if ($detect->isMobile() && !$detect->isTablet())
+    {
+      if (get_option('the_mobile_site_uri'))
+      {
+        $the_mobile_site_uri=get_option('the_mobile_site_uri');
+      }
+      else
+      {
+        $the_mobile_site_uri='/'; // Fallback
+      }
+      $title = apply_filters('widget_title',$instance['title']);
+      $class = "mobile-site-opt mobile-site-link";
+      echo $args['before_widget'];
+      if (!empty($title))
+      {
+        $mobsitelink = '<a class="'.$class.'" href="'.$the_mobile_site_uri.'" title="'.$title.'">'.$title.'</a>';
+      }
+      else
+      {
+        $mobsitelink = '<a class="'.$class.'" href="'.$the_mobile_site_uri.'" title="View Mobile Version">View Mobile Version</a></div>';
+      }
+      echo __($mobsitelink,'text_domain');
+      echo $args['after_widget'];
+    }
+  }
+#   Back-end widget form
+  public function form($instance)
+  {
+    if (isset($instance['title']))
+    {
+      $title = $instance['title'];
+    }
+    else {
+      $title = __('View Mobile Version','text_domain');
+    }
+    ?>
+    <p><label for="<?php echo $this->get_field_id('title'); ?>"><?php _e('Title:'); ?></label>
+    <input class="widefat" id="<?php echo $this->get_field_id('title'); ?>" name="<?php echo $this->get_field_name('title'); ?>" type="text" value="<?php echo esc_attr($title); ?>" />
+    </p>
+<?php
+  }
+#   Sanitize widget form values as they are saved
+  public function update( $new_instance, $old_instance )
+  {
+    $instance = array();
+    $instance['title'] = (!empty($new_instance['title'])) ? strip_tags( $new_instance['title']) : '';
+    return $instance;
+  }
+}
+function reg_avrg_mobsitelink_widget() {
+    register_widget('avrg_mobsitelink_widget');
+}
+add_action('widgets_init','reg_avrg_mobsitelink_widget');
+
+
+
+/*
+    S H O R T C O D E S
+    ===================
+*/
+
+#   [mobilesitelink text="mobile website" class="mobile-site-link"]
+function avrgmobdtct_mobsitelink_sc($atts)
+{
+  if(get_option('the_mobile_site_uri'))
+  {
+    $the_mobile_site_uri=get_option('the_mobile_site_uri');
+  }
+  ob_start();
+  $avrgmobdtctLinkMerged = shortcode_atts( array(
+    'text'    => 'mobile website',
+    'class'   => 'mobile-site-link',
+    'page'    => '',
+  ), $atts, 'mobilesitelink' );
+  extract($avrgmobdtctLinkMerged);
+  if(!empty($page))
+  {
+    if($page=='/')
+    {
+      $mobPageURN = '';
+    }
+    else
+    {
+      $mobPageURN = '/'.$page;
+    }
+  }
+  else
+  {
+    $mobPageURN = $_SERVER['REQUEST_URI'];
+  }
+  echo '<a href="'.$the_mobile_site_uri.$mobPageURN.'" class="'.$class.'">'.$text.'</a>';
+  return ob_get_clean();
+}
+add_shortcode('mobilesitelink', 'avrgmobdtct_mobsitelink_sc');
+
+#   [mobilesitebutton text="View Mobile Version" class="mobile-site-button"]
+function avrgmobdtct_mobsitebttn_sc($atts)
+{
+  if(!class_exists('Mobile_Detect'))
+  {
+    include plugin_basename('/Mobile_Detect.php');
+  }
+  $detect = new Mobile_Detect();
+  if ($detect->isMobile() && !$detect->isTablet())
+  {
+    if(get_option('the_mobile_site_uri'))
+    {
+      $the_mobile_site_uri=get_option('the_mobile_site_uri');
+    }
+    ob_start();
+    $avrgmobdtctBttnMerged = shortcode_atts( array(
+      'text'    => 'View Mobile Version',
+      'class'   => 'mobile-site-button',
+      'page'    => '',
+    ), $atts, 'mobilesitebutton' );
+    extract($avrgmobdtctBttnMerged);
+    if(!empty($page))
+    {
+      if($page=='/')
+      {
+        $mobPageURN = '';
+      }
+      else
+      {
+        $mobPageURN = '/'.$page;
+      }
+    }
+    else
+    {
+      $mobPageURN = $_SERVER['REQUEST_URI'];
+    }
+    echo '<a href="'.$the_mobile_site_uri.$mobPageURN.'" class="'.$class.'">'.$text.'</a>';
+    return ob_get_clean();
+  }
+}
+add_shortcode('mobilesitebutton', 'avrgmobdtct_mobsitebttn_sc');
+
+#   [mobilesite text="View Mobile Version" class="mobile-site-link"]
+function avrgmobdtct_mobsite_sc($atts)
+{
+  if(!class_exists('Mobile_Detect'))
+  {
+    include plugin_basename('/Mobile_Detect.php');
+  }
+  $detect = new Mobile_Detect();
+  if ($detect->isMobile() && !$detect->isTablet())
+  {
+    if(get_option('the_mobile_site_uri'))
+    {
+      $the_mobile_site_uri=get_option('the_mobile_site_uri');
+    }
+    ob_start();
+    $avrgmobdtctScMerged = shortcode_atts( array(
+      'text'    => 'View Mobile Version',
+      'class'   => 'mobile-site-link',
+      'page'    => '',
+    ), $atts, 'mobilesite' );
+    extract($avrgmobdtctScMerged);
+    if(!empty($page))
+    {
+      if($page=='/')
+      {
+        $mobPageURN = '';
+      }
+      else
+      {
+        $mobPageURN = '/'.$page;
+      }
+    }
+    else
+    {
+      $mobPageURN = $_SERVER['REQUEST_URI'];
+    }
+    echo '<a href="'.$the_mobile_site_uri.$mobPageURN.'" class="'.$class.'">'.$text.'</a>';
+    return ob_get_clean();
+  }
+}
+add_shortcode('mobilesite', 'avrgmobdtct_mobsitebttn_sc');
+
+
+
+/*
+  M E T A B O X
+  =============
+*/
+
+add_action('add_meta_boxes', 'avrgmobdtct_add');
+function avrgmobdtct_add()
+{
+  add_meta_box('avrgmobdtctMetaBox', 'Mobile Detect', 'avrgmobdtct_cb', '', 'normal', 'high');
+}
+function avrgmobdtct_cb($post)
+{
+  $values = get_post_custom($post->ID);
+  $mobile_equivlant = isset( $values['avrgmobdtct_equiv']) ? esc_attr($values['avrgmobdtct_equiv'][0]) : '';
+  wp_nonce_field('avrgmobdtct_nonce', 'meta_box_nonce');
+  ?>
+  <p>
+    <label for="avrgmobdtct_equiv">Mobile Equivlant:</label>
+    <input type="text" class="large-text" name="avrgmobdtct_equiv" id="avrgmobdtct_equiv" value="<?php echo $mobile_equivlant; ?>" />
+    <p class="description">The mobile version of this page, which you want to redirect mobile devices to. This is not necessary unless the URN/slug is different than it is on this desktop version. Start it with a slash and omit the domain name. Example: /about-author.php</p>
+  </p>
+  <?php
+}
+add_action( 'save_post', 'avrgmobdtct_save' );
+function avrgmobdtct_save( $post_id )
+{
+  if( defined( 'DOING_AUTOSAVE' ) && DOING_AUTOSAVE ) return;
+  if( !isset( $_POST['meta_box_nonce'] ) || !wp_verify_nonce( $_POST['meta_box_nonce'], 'avrgmobdtct_nonce' ) ) return;
+  if( !current_user_can( 'edit_post' ) ) return;
+  if( isset( $_POST['avrgmobdtct_equiv']))
+    update_post_meta($post_id, 'avrgmobdtct_equiv', $_POST['avrgmobdtct_equiv']);
+}
---
+++ readme.txt
@@ -0,0 +1,181 @@
+
+=== Average Mobile Detect ===
+
+Contributors: addfunc,average.technology,joerhoney
+Donate link: https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=7AF7P3TFKQ2C2
+Tags: mobile, mobile detect, mobile detection, mobile redirect, mobile site redirect, mobile redirection, mobile site redirection, 301 redirect to mobile site, mobile device, redirect mobile device, desktop site to mobile site redirect
+Requires at least: 3.0.1
+Tested up to: 4.0
+Stable tag: 1.1
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+Redirects mobile traffic to mobile site, allows visitors to opt for desktop site, provides shortcodes and widget to generate links to mobile site
+
+== Description ==
+
+Uses [Mobile Detect](http://mobiledetect.net/) to redirect mobile traffic to your mobile website.
+
+*   Redirects on a page-by-page basis (posts and custom post types included). For example; `yourwebsite.com/contact/` will redirect mobile devices to `yourmobilewebsite.com/contact/` instead of always redirecting to your mobile home page.
+
+*   This can be overridden on any page individually with a convenient meta box adjacent to the WYSIWYG (usually underneath, by default).
+
+*   Sets a cookie to remember which version of your website (desktop or mobile, usually) your visitors opted for (so it's okay to set a link on your mobile site to view the desktop version).
+
+*   Includes a widget for inserting a link back to your mobile site, which is only generated for mobile devices.
+
+*   Includes two shortcodes for generating links to your mobile site. One is generated only for mobile devices and the other is generated regardless.
+
+*   No CSS rules are used and classes are provided, yielding coders full reign to style generated links to fit the website theme. Shortcodes even allow you to set the link's classes.
+
+*   Adds a class to the `<body>` ("`mobile-detected`") to help coders write styles specifically for mobile devices.
+
+*   Leaves 404 errors untouched, allowing you to maintain 404 statuses.
+
+Basically, it gives you loads of control of your mobile redirects.
+
+**Custom support tickets are available**
+
+See [Other Notes][1] tab for details.
+
+[1]: http://wordpress.org/plugins/average-mobile-detect/other_notes/
+
+== Installation ==
+
+1. Upload the entire `average-mobile-detect` directory to the `/wp-content/plugins/` directory of the desktop website (the website doing the redirecting)
+2. Activate the plugin through the 'Plugins' menu in WordPress
+3. Enter the URL you want to redirect mobile devices to in Settings > Mobile Detect
+4. Confirm the auto-populated URL of the desktop website is correct or override it.
+5. Test, as one always should. :)
+
+== Frequently Asked Questions ==
+
+= What's so great about a "page-by-page" redirecting? =
+
+This can help search engines understand the relationship between your desktop and mobile website. It also makes the mobile user experience much more fluid when clicking a link to a particular page on your website (they don't get dump onto your mobile home page, where they then have to figure out where the page they were looking for is).
+
+= What's wrong with redirecting to the home/front page? =
+
+1. If you have links published in your marketing campaign that go directly to individual pages of your website, you probably want those links to take visitors to those pages respectively, mobile users included. If you don't have such links published, search engines do.
+2. Redirecting all search engines to your home page can lower its status, as it can make your home page appear to be a 404 page.
+
+= What's the point of having optional links to the mobile website? =
+
+Suppose your mobile website has a link to your desktop website (e.g. "View desktop version" or "Full Site") and the user clicks it. Average Mobile Detect sets a cookie to remember that the visitor has opted for the desktop website. Now let's say your visitor changed his/her mind and wants the mobile site again. A link to the mobile site is the obvious solution, but in most cases you wouldn't want that link to show up on desktop computers, so we don't generate it. Conveniently, these generated links automatically use whatever URL you enter in on the settings page, so even if you later have your mobile website at another URL, you'll never need to directly update these links.
+
+= What about tablets? =
+
+Currently, tablets are treated as desktops. Generally they do a fine job of displaying desktop websites. However, in a future release, there may be a setting to treat tablets as mobile devices if desired. In this case, the default for tablets will always remain to be regarded as desktops.
+
+= I activated the plugin and nothing happened. =
+
+All options are turned OFF by default to avoid unwanted surprises. Be sure to set the domain name of your mobile website in Settings > Mobile Detect, before activating the redirect function.
+
+= Does it really require WordPress 3.0.1 or later? =
+
+I have not tested it on earlier versions. In fact, I could use help with testing. Feel free to try it out in an earlier version and let me know if it works! :)
+
+= Does Average have a website =
+
+When a coconut falls on the beach and no one is around to hear it, does it make a sound... I mean no. Not yet.
+
+== Screenshots ==
+
+1. Set the URL to your mobile website and activate. The URI of your desktop website should be automatically populated, but if for whatever reason you need to change it just enter it into the appropriate field. This page also serves as a reference for the shortcodes included.
+
+2. An optional widget also generates a link to your mobile website. The link displays only on mobile devices.
+
+3. Pages and post (and custom post types) are equipped with a meta box for overriding its own mobile redirect location (e.g. if the page on your desktop site is `yourdesktopsite.com/sales`, but on your mobile site it isn't `yourmobilesite.com/sales`, but `yourmobilesite.com/mobilespecials` instead, you can override the redirect with `/mobilespecials` in this field.
+
+== Changelog ==
+
+= 1.1 =
+15-Oct-2014
+
+*   Adds shortcode [mobilesite] back in for older websites that are using it (websites created before Average Mobile Detect was released on the WordPress repository)
+*   Adds CSS class "mobile-site-opt" to link output by widget to replace css class "mobile-site-link"
+*   Declares CSS class "mobile-site-link" deprecated for widgets (not deprecated for shortcode). Use "mobile-site-opt" instead. This was decided upon to avoid confusion between the widget and the shortcode when styling.
+
+= 1.0 =
+27-Aug-2014
+
+*   Prepared for release
+
+= 0.8 =
+25-Aug-2014
+
+*   Removes shortcode [mobilesite]
+*   Adds shortcode [mobilesitelink]. Parameters: text, class and page
+*   Adds shortcode [mobilesitebutton] (display on mobile devices only). Parameters: text, class and page
+
+= 0.7.2 =
+20-Aug-2014
+
+*   Changes database field names back, as that would be an unnecessary hassle for previous installs. Fields changed are:
+  -   mobile_site_uri changed back to:  the_mobile_site_uri
+	-   desktop_site_uri changed back to:  non_mobile_site_uri
+*   Removes metabox to prepare plugin for release
+
+= 0.7.1 =
+22-Jul-2014
+
+*   Redirects desktop page to equivlant mobile page instead of always the mobile Home page (example: website.com/about/ => m.website.com/about/)
+*   No longer redirects 404 errors (for better or for worse, but this was a side effect of the above)
+*   Changes curtain database field names, so upgrading must be backed up and done carefully. Fields changed are:
+  -    the_mobile_site_uri  changed to:  mobile_site_uri
+	-    non_mobile_site_uri  changed to:  desktop_site_uri
+*   Adds a metabox for specifying a particular page on the mobile site to redirect the desktop version of the page to (saves the data, but doesn't do anything with it yet)
+
+= 0.7 =
+26-Jun-2014
+
+*   Now checks if class Mobile_Detect has been declared already before attempting to declare it
+
+= 0.6 =
+2-Jun-2014
+
+*   No longer redirects tables to the mobile website (needs to be made optional in the future)
+
+= 0.5 =
+11-Feb-2014
+
+*   Adds class .mobile-detected to body if isMobile() returns true
+
+= 0.4 =
+2-Dec-2013
+
+*   Removes "This is the Mobile Site" ($is_the_mobile_site), hence eliminating the need to install this plugin on the mobile website
+*   Adds [mobilesite] shortcode. Takes text="Optional" (overrides default link text: "View Mobile Version") and classes="optional" (overrides default class: "mobile-site-link")
+
+= 0.3 =
+2-Dec-2013
+
+*   FIX: All known bugs, except subdomains still have limited support:
+  1.   This version of Average Mobile Detect does not fully support subdomains in the Non-mobile Website URI field. This field is used to set the cookie for your non-mobile website, which tells it whether to redirect the device or not. However, you can still use a website that resides on a subdomain. To do this, omit all characters that come before the first period, but leave the period there (example: .your-website.com). Be aware that this will set the cookie for all subdomains pertaining to the root domain name (so in our example, the cookie would be set for all of these: m.your-website.com, www.your-website.com, other.your-website.com, even-unregistered-subdomains.your-website.com, etc.)
+  2.   As the redirect is governed by a cookie set/changed by both the mobile website and the non-mobile website entered above, the "Non-mobile Website URI" feild must be filled in exactly the same on both websites. (This plugin has to be installed and active on both websites.)
+*   Mobile Site Link doubles as link to non-mobile site as entered in Non-mobile Website URI if "This is the Mobile Website" is set to "Yes". Due to the above limited support however, this doesn't work when using a subdomain
+
+= 0.2 =
+26-Nov-2013
+
+*   Adds Mobile Site Link widget which generates a link to the mobile site (for mobile devices only)... and a lot of bugs
+
+= 0.1 =
+19-Nov-2013
+
+*   Uses MobileDetect.php to detect whether device is mobile or not and redirects mobile users to another location (specified in Settings)
+*   Sets a cookie to track whether the mobile user wants the mobile site or the full site. Cookie expires on browser close
+
+== Custom Support ==
+
+If you have a custom support need, [please purchase your support ticket here](https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=7AF7P3TFKQ2C2). Support tickets are responded to within 24 hours, but we answer them as soon as possible.
+
+**How it works**
+
+1.   [Purchase a support ticket via PayPal](https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=7AF7P3TFKQ2C2)
+2.   You get a chance to provide the best way to contact you and a description of your need
+3.   I contact you as soon as I can (no less than 24 hours) and help resolve your issue
+
+**Note:** This is for custom needs for help, not problems with the plugin, or instructions that should already be explain in the description. If you feel there are important details omitted from the description, installation steps, etc. of the plugin, please report them in the Support forum. Thanks!
+
+== Upgrade Notice ==
